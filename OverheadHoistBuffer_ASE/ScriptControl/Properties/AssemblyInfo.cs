0ec3a24b93c33034f72bbee066c7efc483fedaea
kevin-wei-0728 <kivin19882003@gmail.com>
2020-09-30 12:08:30 +0800

    與志昌的版本合併後，更新至v1.5.1

--- OverheadHoistBuffer_ASE/ScriptControl/Properties/AssemblyInfo.cs
+++ OverheadHoistBuffer_ASE/ScriptControl/Properties/AssemblyInfo.cs
@@ -32,12 +32,12 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyFileVersion("1.5.1")]
 
 //版號 + Tab + 日期 + Tab + 修改人 + Tab + 敘述
+//1.5.1 2020/09/30  SCC+    增加命令狀態為 Canceling、Aborting 進行強制結束功能
 //1.5.0 2020/09/28  JasonWu+ 修改PortValueDefMapAction 中對於LoadPostion1 的觸發，要同時判斷是否為Auto mode。
-//1.4.10    2020/09/30  SCC+    增加命令狀態為 Canceling、Aborting 進行強制結束功能
 //1.4.9 2020/09/23  JasonWu+ 1.修改判定 port 是否可用之判定邏輯，拿掉 port ready 及 mismatch 訊號。 
 //                           2.修改判定是否可轉向，進一步確認是否為實盒及out mode 避免出貨命令被取走。
 //                           3.修改原判定退補空盒命令產生時間，由 10 -> 3.5 (因為AGVC 每3秒觸發一次)
