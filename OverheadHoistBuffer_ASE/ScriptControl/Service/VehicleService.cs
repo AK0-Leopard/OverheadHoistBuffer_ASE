5c4d622a17c0fbb5a63d0e9330eb86d19278a1af
kevinwei <kivin19882003@gmail.com>
2022-01-29 06:40:50 +0800

    更新版本至1.6.55，加入當車子上報Loading/Unloading時， 若上報多次也會重新啟動對於該車子的通訊section

--- OverheadHoistBuffer_ASE/ScriptControl/Service/VehicleService.cs
+++ OverheadHoistBuffer_ASE/ScriptControl/Service/VehicleService.cs
@@ -131,6 +131,8 @@ namespace com.mirle.ibg3k0.sc.Service
                    Data: $"Over {AVEHICLE.MAX_ALLOW_IMPORTANT_EVENT_RETRY_COUNT} times report important:{overRetryImportantEvent}, begin force close tcpip section ...",
                    VehicleID: vh.VEHICLE_ID,
                    CarrierID: vh.CST_ID);
+                scApp.TransferService.TransferServiceLogger.
+                    Info($"Over {AVEHICLE.MAX_ALLOW_IMPORTANT_EVENT_RETRY_COUNT} times report important:{overRetryImportantEvent}, begin force close tcpip section ...");
 
                 vh.StopTcpIpConnection(scApp.getBCFApplication());
             }
@@ -317,7 +319,7 @@ namespace com.mirle.ibg3k0.sc.Service
         public void stopVehicleTcpIpSessionTest(string vhID)
         {
             AVEHICLE vh = scApp.VehicleBLL.cache.getVhByID(vhID);
-            vh.StopTcpIpConnection(scApp.getBCFApplication());
+            vh.StopTcpIpConnection(scApp.getBCFApplication());3+
         }
         public bool stopVehicleTcpIpServer(string vhID)
         {
